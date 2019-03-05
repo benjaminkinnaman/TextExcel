53e12031c50b49f5fb50656fb8d4d2d22cb2d46d
APCS3rd <APCS3rd@MHS-Dreyer-11>
2019-03-05 10:48:10 -0800

    3/5 test commit

---
+++ TextExcel/src/textExcel/EmptyCell.java
@@ -0,0 +1,18 @@
+//@author Benjamin Kinnaman
+//@version March 4, 2019
+
+package textExcel;
+
+public class EmptyCell implements Cell{
+	//text for spreadsheet cell display, must be exactly length 10
+	public String abbreviatedCellText() {
+		return "";
+	}
+	//text for individual cell
+	
+	
+	
+	
+	
+	
+}
---
+++ TextExcel/src/textExcel/TextExcel.java
@@ -0,0 +1,18 @@
+package textExcel;
+
+import java.io.FileNotFoundException;
+import java.util.*;
+
+// Update this file with your own code.
+
+public class TextExcel
+{
+
+	public static void main(String[] args)
+	{
+	    // Add your command loop here
+		Scanner consoleInput = new Scanner(System.in);
+		System.out.print("Enter a command: ");
+		String userInput = console.getInt
+	}
+}
