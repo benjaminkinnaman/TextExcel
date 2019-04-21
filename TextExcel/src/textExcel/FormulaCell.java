6ad5bccd17d5378dd5e6e66ea024c4911c4b39bd
theki <theki@DESKTOP-8E3TBPC>
2019-04-21 16:37:45 -0700

    4-21 late and gay

--- TextExcel/src/textExcel/FormulaCell.java
+++ TextExcel/src/textExcel/FormulaCell.java
@@ -25,11 +25,14 @@ public class FormulaCell extends RealCell {
 		String[] splitFormula = text.split(" ");
 		double answer = Double.parseDouble(splitFormula[1]);
 		
-		if (splitFormula[1].equals("SUM")) {
+		if (splitFormula[1].equals("SUM")) {	//Find the sum of the range of cells added together
 			
-		} else if (splitFormula[1].equals("AVG")) {
+		} else if (splitFormula[1].equals("AVG")) {	//Find the average of the cells stated.
+			//Logic: Add together, and divide by the number of cells. Code from SUM may be reusable.
 			
 		} else {
+		//This is an arithmetic problem. Find the appropriate function and procedd with execution.
+		//REMEMBER! Multiple functions may be usable. Solve in groups of two. May require recalling methods.
 		for (int i = 2; i < splitFormula.length - 1; i+=2) {
 				if (splitFormula[i].equals("+")) {
 					answer += Double.parseDouble(splitFormula[i+1]);
